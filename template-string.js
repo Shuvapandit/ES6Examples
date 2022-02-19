391a52dbe5eefda728086ea6c6107766f8c770f9
shuva <chakrabortyshuva216@gmail.com>
2022-02-20 02:39:05 +0600

    commit ` in es6

---
+++ template-string.js
@@ -0,0 +1,18 @@
+const x="shuva chakraborty";
+const y=' helo developer';
+//for multiline we use ` ` in es6
+const z=` Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.`
+console.log(z);
+const count=5;
+/* const o='<h1>ES-5</h1>';
+const o2='<h1>ES- '+count +'</h1>'; */
+// `` in es6
+const new02=`<h1>ES- ${count}</h1>`
+console.log(new02);
+
+    const fname="shuva"
+    const title="cb";
+        //const fullname= fname +' ' +title;
+        const newfullname=` This person name is:${fname} ${title}`
+
+console.log(newfullname);
