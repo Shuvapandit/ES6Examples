91e00c42927b0c5963476e804fef2639694fc314
shuva <chakrabortyshuva216@gmail.com>
2022-02-20 19:09:11 +0600

    commit more arrow function examples

--- arrow-function.js
+++ arrow-function.js
@@ -18,3 +18,21 @@ console.log(sum); */
 const add = (num1,num2) => num1+num2;
 const sum=add(12,14);
 console.log(sum)
+//you can call arrow function without any parameter for one parameter
+const fivestimes=num =>num *5;
+const multiply=fivestimes(12);
+console.log(multiply)
+
+const getname=()=>'shuva';
+const v=getname();
+console.log(v);
+
+const domath=(p,k)=>{
+    const add=p+k;
+    const minus=p-k;
+    const result=add*minus;
+    const output=result*20;
+    return output;
+}
+const outputIS=domath(3,2);
+console.log(outputIS);
