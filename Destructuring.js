7729b94a32cb545623cbe000e9a3812e084c46cb
shuva <chakrabortyshuva216@gmail.com>
2022-02-20 21:51:58 +0600

    commit Destructuring in ES6

---
+++ Destructuring.js
@@ -0,0 +1,9 @@
+const hilsha={id:90,Name:"Hilsha", phone:'017324843' , address:'chandpur'}
+const {id,Name,phone,address}=hilsha;
+console.log(id,phone)
+console.log(id,Name)
+
+const company={CompanyName:'softeach',CEO:{id:1,Nam:'shuva'},web:{work:"frontEnd developer",framework:"react"}};
+const {work,framework}=company.web;
+const {Nam}=company.CEO;
+console.log(Nam,work,framework)
