f6b32933e836448d968bf87ee76751e110e19f76
shuva <chakrabortyshuva216@gmail.com>
2022-02-20 14:14:27 +0600

    commit arrow function in es6

---
+++ arrow-function.js
@@ -0,0 +1,20 @@
+
+/*  //function expression
+const add =function add(num1,numb2){
+    return num1+numb2;
+}
+console.log(add(10,12))
+
+//function expression(annonymous)
+const addnew=function (nam1,nam2)
+{
+    return nam1+nam2;
+}
+const sum =addnew(10,1)
+
+console.log(sum); */
+
+//arrow function 
+const add = (num1,num2) => num1+num2;
+const sum=add(12,14);
+console.log(sum)
