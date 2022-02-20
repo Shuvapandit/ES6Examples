d6fd98952fa7021469f7fd447cf990ecf54026a6
shuva <chakrabortyshuva216@gmail.com>
2022-02-20 19:48:54 +0600

    commit spreed operator in es6

---
+++ spreed.js
@@ -0,0 +1,8 @@
+const numbers=[10,20,30,40,50];
+/* console.log(numbers)
+console.log(...numbers) */
+const max=Math.max(21,33,99,33,3333);
+const maxinarray=Math.max(...numbers);
+console.log(maxinarray)
+const numbers2=[22,...numbers,99]
+console.log(numbers2)
