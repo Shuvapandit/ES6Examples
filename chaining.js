4043d393723526b624ea50b8c53bbcfa3c4321e0
shuva <chakrabortyshuva216@gmail.com>
2022-02-21 12:02:28 +0600

    commit destructuring more in js

---
+++ chaining.js
@@ -0,0 +1,16 @@
+const myobj={x:20,y:303,z:00,l:44}
+const {z,l}=myobj;
+console.log(l)
+const [x,y]=['shuva', 'chakraborty'];
+console.log(x,y);
+//
+const company={CompanyName:'softeach',
+CEO:
+{id:1,Nam:'shuva'},
+
+web:
+{work:"frontEnd developer",framework:"react",
+teach:{first:'html',second:'css'}}};
+
+console.log(company.web.teach.second) 
+console.log(company.web.work.teach?.second) 
